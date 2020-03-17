4e7b6ee1ca6bffe4698c329364d72c4a4e3385ef
Bachmann Maximilian (LEG) <maximilian.bachmann@liebherr.com>
2020-03-17 16:09:24 +0100

    fix wrong namespace

--- cpp/src/process.hpp
+++ cpp/src/process.hpp
@@ -13,7 +13,7 @@ std::optional<std::pair<float, std::string>> extract_one(std::string query, std:
   float max_score = 0;
   std::string result_choice;
   for (const auto &choice : choices) {
-    float score = ratio(query, choice, score_cutoff);
+    float score = fuzz::ratio(query, choice, score_cutoff);
     if (score > score_cutoff) {
       score_cutoff = score;
       max_score = score;
