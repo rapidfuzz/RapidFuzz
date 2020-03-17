df7129877280d0620ba990ca659104a602a22e73
maxbachmann <44199644+maxbachmann@users.noreply.github.com>
2020-03-17 12:14:48 +0100

    provide default arguments

--- python/rapidfuzz.cpp
+++ python/rapidfuzz.cpp
@@ -2,15 +2,20 @@
 #include <pybind11/stl.h>
 #include "process.hpp"
 
+namespace py = pybind11;
 
 PYBIND11_MODULE(rapidfuzz, m) {
     m.doc() = R"pbdoc(
         rapid string matching library
     )pbdoc";
 
-    m.def("extract_one", &extract_one, R"pbdoc(
-        Find the best match in a list of matches
-    )pbdoc");
+    auto mprocess = m.def_submodule("process");
+ 
+    mprocess.def("extractOne", &extract_one,
+                 py::arg("query"), py::arg("choices"), py::arg("score_cutoff") = 0
+                 R"pbdoc(
+                    Find the best match in a list of matches
+                 )pbdoc");
 
     m.attr("__version__") = VERSION_INFO;
 }
