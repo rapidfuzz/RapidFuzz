54609c75081b8f8ecbbb5dc6e2a3a5544a2627e6
maxbachmann <ssh@maxbachmann.de>
2020-03-30 21:40:20 +0200

    add benchmark

--- Benchmarks.md
+++ Benchmarks.md
@@ -1,1 +1,18 @@
-Will be added soon
+# Benchmarks
+
+To compare the speed of FuzzyWuzzy and RapidFuzz the Benchmark of FuzzyWuzzy is used.
+Therefore the Benchmark is always executed for both FuzzyWuzzy and RapidFuzz.
+Afterwards a ratio between the runtime of both results is calculated. The benchmark can be found [here](https://github.com/rhasspy/rapidfuzz/blob/master/python/bench). The results of the benchmarks are visualised below.
+
+## fuzz.ratio
+
+![](.github/fuzz.ratio.png)
+
+
+## fuzz.partial_ratio
+
+![](.github/fuzz.partial_ratio.png)
+
+## fuzz.WRatio
+
+![](.github/fuzz.WRatio.png)
---
+++ python/bench/benchmark.py
@@ -0,0 +1,137 @@
+from timeit import timeit
+import math
+import csv
+
+iterations = 100000
+
+
+reader = csv.DictReader(open('titledata.csv'), delimiter='|')
+titles = [i['custom_title'] for i in reader]
+title_blob = '\n'.join(titles)
+
+
+cirque_strings = [
+    "cirque du soleil - zarkana - las vegas",
+    "cirque du soleil ",
+	"cirque du soleil",
+    "cirque du soleil las vegas",
+    "zarkana las vegas",
+    "las vegas cirque du soleil at the bellagio",
+    "zarakana - cirque du soleil - bellagio"
+]
+
+choices = [
+    "",
+    "new york yankees vs boston red sox",
+    "",
+    "zarakana - cirque du soleil - bellagio",
+    None,
+    "cirque du soleil las vegas",
+    None
+]
+
+mixed_strings = [
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    "C\\'est la vie",
+    u"Ça va?",
+    u"Cães danados",
+    u"\xacCamarões assados",
+    u"a\xac\u1234\u20ac\U00008000"
+]
+
+common_setup = "from {} import fuzz, process; "
+
+
+def print_result_from_timeit(stmt='pass', setup='pass', number=1000000):
+    """
+    Clean function to know how much time took the execution of one statement
+    """
+    units = ["s", "ms", "us", "ns"]
+
+    setup_fuzzywuzzy = setup.format("fuzzywuzzy")
+    duration_fuzzywuzzy = timeit(stmt, setup_fuzzywuzzy, number=int(number))
+    avg_duration = duration_fuzzywuzzy / float(number)
+    thousands = int(math.floor(math.log(avg_duration, 1000)))
+
+    print("Total time: %fs. Average run: %.3f%s." % (
+        duration_fuzzywuzzy, avg_duration * (1000 ** -thousands), units[-thousands]))
+    
+    setup_rapidfuzz = setup.format("rapidfuzz")
+    duration_rapidfuzz = timeit(stmt, setup_rapidfuzz, number=int(number))
+    avg_duration = duration_rapidfuzz / float(number)
+    thousands = int(math.floor(math.log(avg_duration, 1000)))
+
+    print("Total time: %fs. Average run: %.3f%s." % (
+        duration_rapidfuzz, avg_duration * (1000 ** -thousands), units[-thousands]))
+    
+    relative_duration = duration_fuzzywuzzy / duration_rapidfuzz
+    print("RapidFuzz is %.3f times faster than FuzzyWuzzy" % relative_duration)
+
+    print()
+
+
+# benchmarking the core matching methods...
+
+for s in cirque_strings:
+    print('Test fuzz.ratio for string: "%s"' % s)
+    print('-------------------------------')
+    print_result_from_timeit('fuzz.ratio(u\'cirque du soleil\', u\'%s\')' % s,
+                             common_setup, number=iterations / 100)
+
+for s in cirque_strings:
+    print('Test fuzz.partial_ratio for string: "%s"' % s)
+    print('-------------------------------')
+    print_result_from_timeit('fuzz.partial_ratio(u\'cirque du soleil\', u\'%s\')' % s,
+                             common_setup, number=iterations / 100)
+
+for s in cirque_strings:
+    print('Test fuzz.WRatio for string: "%s"' % s)
+    print('-------------------------------')
+    print_result_from_timeit('fuzz.WRatio(u\'cirque du soleil\', u\'%s\')' % s,
+                             common_setup, number=iterations / 100)
+
+print('Test process.extract(scorer =  fuzz.QRatio) for string: "%s"' % s)
+print('-------------------------------')
+print_result_from_timeit('process.extract(u\'cirque du soleil\', choices, scorer =  fuzz.QRatio)',
+                             common_setup + " import string,random; random.seed(18);"
+                             " choices = [\'\'.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(30)) for s in range(5000)]",
+                              number=10)
+
+print('Test process.extract(scorer =  fuzz.WRatio) for string: "%s"' % s)
+print('-------------------------------')
+print_result_from_timeit('process.extract(u\'cirque du soleil\', choices, scorer =  fuzz.WRatio)',
+                             common_setup + " import string,random; random.seed(18);"
+                             " choices = [\'\'.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(30)) for s in range(5000)]",
+                              number=10)
+
+print('Test process.extractOne(scorer =  fuzz.WRatio) for string: "%s"' % s)
+print('-------------------------------')
+print_result_from_timeit('process.extractOne(u\'cirque du soleil\', choices, scorer =  fuzz.WRatio)',
+                             common_setup + " import string,random; random.seed(18);"
+                             " choices = [\'\'.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(30)) for s in range(5000)]",
+                              number=10)
+
+print('Test process.extractOne(scorer =  fuzz.WRatio, score_cutoff=90) for string: "%s"' % s)
+print('-------------------------------')
+print_result_from_timeit('process.extractOne(u\'cirque du soleil\', choices, scorer =  fuzz.WRatio, score_cutoff=90)',
+                             common_setup + " import string,random; random.seed(18);"
+                             " choices = [\'\'.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(30)) for s in range(5000)]",
+                              number=10)
+
+
+# let me show you something
+
+s = 'New York Yankees'
+
+test = 'import functools\n'
+test += 'title_blob = """%s"""\n' % title_blob
+test += 'title_blob = title_blob.strip()\n'
+test += 'titles = title_blob.split("\\n")\n'
+
+print('Real world ratio(): "%s"' % s)
+print('-------------------------------')
+test += 'prepared_ratio = functools.partial(fuzz.ratio, "%s")\n' % s
+test += 'titles.sort(key=prepared_ratio)\n'
+print_result_from_timeit(test,
+                         common_setup,
+                         number=100)
