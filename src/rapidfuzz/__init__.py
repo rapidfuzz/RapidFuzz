865fbf0d8a1bfceddbccea4abc1061171f703859
maxbachmann <ssh@maxbachmann.de>
2020-10-01 22:42:58 +0200

    fix inconsistency towards fuzzywuzzy

--- src/rapidfuzz/__init__.py
+++ src/rapidfuzz/__init__.py
@@ -3,6 +3,6 @@ rapid string matching library
 """
 __author__ = "Max Bachmann"
 __license__ = "MIT"
-__version__ = "0.12.1"
+__version__ = "0.12.2"
 
 from rapidfuzz import process, fuzz, levenshtein, utils
