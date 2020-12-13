40ed0a2cb55b0ece92b3ebd81a4d6697811dd133
Max Bachmann <kontakt@maxbachmann.de>
2020-12-13 17:07:01 +0100

    fix rebase

--- src/rapidfuzz/__init__.py
+++ src/rapidfuzz/__init__.py
@@ -3,10 +3,6 @@ rapid string matching library
 """
 __author__ = "Max Bachmann"
 __license__ = "MIT"
-<<<<<<< HEAD
-__version__ = "0.14.1"
-=======
 __version__ = "1.0.0"
->>>>>>> d356e54 (support kwargs in process.extract)
 
 from rapidfuzz import process, fuzz, utils, levenshtein, string_metric
