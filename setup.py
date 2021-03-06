53b8e3bd615f711fac380968f9a1cb7fd7afcd40
Max Bachmann <kontakt@maxbachmann.de>
2021-03-06 22:52:18 +0100

    update build mechanism

--- setup.py
+++ setup.py
@@ -2,17 +2,6 @@ from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 import sys
 
-from os import path
-from io import open
-
-this_dir = path.abspath(path.dirname(__file__))
-with open(path.join(this_dir, "VERSION"), encoding='utf-8') as version_file:
-    version = version_file.read().strip()
-
-
-with open(path.join(this_dir, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
 class BuildExt(build_ext):
     """A custom build extension for adding compiler-specific options."""
     c_opts = {
@@ -42,61 +31,49 @@ class BuildExt(build_ext):
             ext.extra_link_args += link_opts
         build_ext.build_extensions(self)
 
-setup(
-    name='rapidfuzz',
-    version=version,
-    author='Max Bachmann',
-    author_email='contact@maxbachmann.de',
-    url='https://github.com/maxbachmann/rapidfuzz',
-    description='rapid fuzzy string matching',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    ext_modules = [
-        Extension(
-            'rapidfuzz.cpp_process', [
-                'src/cpp_process.cpp',
-                'src/rapidfuzz-cpp/rapidfuzz/details/unicode.cpp'],
-            include_dirs=["src/rapidfuzz-cpp/"],
-            language='c++',
-        ),
-        Extension(
-            'rapidfuzz.cpp_fuzz', [
-                'src/cpp_fuzz.cpp',
-                'src/rapidfuzz-cpp/rapidfuzz/details/unicode.cpp'],
-            include_dirs=["src/rapidfuzz-cpp/"],
-            language='c++',
-        ),
-        Extension(
-            'rapidfuzz.cpp_string_metric', [
-                'src/cpp_string_metric.cpp',
-                'src/rapidfuzz-cpp/rapidfuzz/details/unicode.cpp'],
-            include_dirs=["src/rapidfuzz-cpp/"],
-            language='c++',
-        ),
-        Extension(
-            'rapidfuzz.cpp_utils', [
-                'src/cpp_utils.cpp',
-                'src/rapidfuzz-cpp/rapidfuzz/details/unicode.cpp'],
-            include_dirs=["src/rapidfuzz-cpp/"],
-            language='c++',
-        ),
-    ],
-    cmdclass={'build_ext': BuildExt},
-    package_data={'': ['LICENSE', 'VERSION']},
-    package_dir={'': 'src'},
-    packages=['rapidfuzz'],
-    include_package_data=True,
-    zip_safe=False,
-    classifiers=[
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "License :: OSI Approved :: MIT License",
-    ],
-    python_requires=">=2.7",
-)
+
+ext_modules = [
+    Extension(
+        name='rapidfuzz.cpp_process',
+        sources=[
+            'src/cpp_process.cpp',
+            'src/rapidfuzz-cpp/rapidfuzz/details/unicode.cpp'
+        ],
+        include_dirs=["src/rapidfuzz-cpp/"],
+        language='c++',
+    ),
+    Extension(
+        name='rapidfuzz.cpp_fuzz',
+        sources=[
+            'src/cpp_fuzz.cpp',
+            'src/rapidfuzz-cpp/rapidfuzz/details/unicode.cpp'
+        ],
+        include_dirs=["src/rapidfuzz-cpp/"],
+        language='c++',
+    ),
+    Extension(
+        name='rapidfuzz.cpp_string_metric',
+        sources=[
+            'src/cpp_string_metric.cpp',
+            'src/rapidfuzz-cpp/rapidfuzz/details/unicode.cpp'
+        ],
+        include_dirs=["src/rapidfuzz-cpp/"],
+        language='c++',
+    ),
+    Extension(
+        name='rapidfuzz.cpp_utils',
+        sources=[
+            'src/cpp_utils.cpp',
+            'src/rapidfuzz-cpp/rapidfuzz/details/unicode.cpp'
+        ],
+        include_dirs=["src/rapidfuzz-cpp/"],
+        language='c++',
+    )
+]
+
+
+if __name__ == "__main__":
+    setup(
+        cmdclass={'build_ext': BuildExt},
+        ext_modules = ext_modules
+    )
