aee5fab9c8fdc0d3ede980e087ca76d55f95401d
Max Bachmann <kontakt@maxbachmann.de>
2021-06-17 14:37:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -48,11 +48,11 @@ class BuildExt(build_ext):
             # multiprocess requires
             if ext.name == 'rapidfuzz.cpp_multiprocess':
                 if sys.platform == 'darwin':
-                    ext.extra_compile_args.append('-Xpreprocessor')
+                    ext.extra_compile_args.append('-Xpreprocessor -fopenmp')
                     ext.extra_link_args.append('-lomp')
                 else:
                     ext.extra_link_args.append('-fopenmp')
-                ext.extra_compile_args.append('-fopenmp')
+                    ext.extra_compile_args.append('-fopenmp')
         build_ext.build_extensions(self)
 
 
