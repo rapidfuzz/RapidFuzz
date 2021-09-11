8a293f154510d0fddcfb5f348d9ec33dbc0fc315
Max Bachmann <kontakt@maxbachmann.de>
2021-09-11 12:27:39 +0200

    Release v1.6.1

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Max Bachmann'
 author = 'Max Bachmann'
 
 # The full version, including alpha/beta/rc tags
-release = '1.6.0'
+release = '1.6.1'
 
 
 # -- General configuration ---------------------------------------------------
