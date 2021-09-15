e40580dd26f6f2a5a93305f5c8c5d5b04d9cff70
Max Bachmann <kontakt@maxbachmann.de>
2021-09-15 09:37:52 +0200

    Release v1.6.2

--- src/rapidfuzz/__init__.py
+++ src/rapidfuzz/__init__.py
@@ -3,6 +3,6 @@ rapid string matching library
 """
 __author__ = "Max Bachmann"
 __license__ = "MIT"
-__version__ = "1.6.1"
+__version__ = "1.6.2"
 
 from rapidfuzz import process, fuzz, utils, levenshtein, string_metric
