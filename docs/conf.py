df568cafbab56359480f0d20b14f8a01b7fef137
Max Bachmann <kontakt@maxbachmann.de>
2021-11-19 09:15:20 +0100

    Add wheels for Python3.6

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Max Bachmann'
 author = 'Max Bachmann'
 
 # The full version, including alpha/beta/rc tags
-release = '1.8.2'
+release = '1.8.3'
 
 
 # -- General configuration ---------------------------------------------------
--- src/rapidfuzz/__init__.py
+++ src/rapidfuzz/__init__.py
@@ -3,6 +3,6 @@ rapid string matching library
 """
 __author__ = "Max Bachmann"
 __license__ = "MIT"
-__version__ = "1.8.2"
+__version__ = "1.8.3"
 
 from rapidfuzz import process, fuzz, utils, levenshtein, string_metric
