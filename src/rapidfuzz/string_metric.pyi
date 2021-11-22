f7a67512eff560f2a8dbe89a385e862123a5b4c3
Jeppe Fihl-Pearson <jeppe@memrise.com>
2021-11-22 16:28:04 +0000

    Add missing import of typing.List

--- src/rapidfuzz/string_metric.pyi
+++ src/rapidfuzz/string_metric.pyi
@@ -1,4 +1,4 @@
-from typing import Callable, Hashable, Sequence, Optional, Union, TypeVar, Tuple
+from typing import Callable, Hashable, Sequence, Optional, Union, TypeVar, Tuple, List
 
 _StringType = Sequence[Hashable]
 S1 = TypeVar("S1")
