0674083701ab8173b9492c43568abc32fa231208
Max Bachmann <kontakt@maxbachmann.de>
2021-12-24 11:58:29 +0100

    cythonize while packaging

---
+++ tools/cythonize.bat
@@ -0,0 +1,5 @@
+cython --cplus src/cpp_utils.pyx -I capi/
+cython --cplus src/cpp_string_metric.pyx -I capi/
+cython --cplus src/cpp_fuzz.pyx -I capi/
+cython --cplus src/cpp_process.pyx -I capi/
+cython --cplus src/cpp_process_cdist.pyx -I capi/
