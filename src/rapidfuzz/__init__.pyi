22a2ecbfa4395b6320c00e491a9ebdf0197d6133
Max Bachmann <kontakt@maxbachmann.de>
2022-01-09 01:17:05 +0100

    add type hints for new functions

--- src/rapidfuzz/__init__.pyi
+++ src/rapidfuzz/__init__.pyi
@@ -1,10 +1,3 @@
 __author__: str
 __license__: str
 __version__: str
-
-from rapidfuzz import (
-    process as process,
-    fuzz as fuzz,
-    utils as utils,
-    string_metric as string_metric,
-)
---
+++ src/rapidfuzz/algorithm/edit_based/JaroWinkler.pyi
@@ -0,0 +1,10 @@
+from typing import Callable, Hashable, Sequence, Optional, Union, TypeVar
+
+_StringType = Sequence[Hashable]
+S1 = TypeVar("S1")
+S2 = TypeVar("S2")
+
+def similarity(
+    s1: S1, s2: S2, *,
+    processor: Optional[Callable[[Union[S1, S2]], _StringType]] = ...,
+    score_cutoff: Optional[float] = 0) -> float: ...
