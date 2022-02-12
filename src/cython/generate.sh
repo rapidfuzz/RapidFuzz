cd8af8cad24aa4b212336eb30d6479432bb3f42a
Max Bachmann <kontakt@maxbachmann.de>
2022-02-12 18:58:10 +0100

    allow generating cython files (#194)

---
+++ src/cython/generate.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+curdir="${0%/*}"
+
+generate_cython()
+{
+  python -m cython -I "$curdir" --cplus "$curdir"/"$1".pyx -o "$curdir"/"$1".cxx
+  echo "Generated $curdir/$1.cxx"
+}
+
+generate_cython cpp_fuzz
+generate_cython cpp_process_cdist
+generate_cython cpp_process
+generate_cython cpp_string_metric
+generate_cython cpp_utils
+
+generate_cython distance/_initialize
+generate_cython distance/Hamming
+generate_cython distance/Indel
+generate_cython distance/Jaro
+generate_cython distance/JaroWinkler
+generate_cython distance/Levenshtein
