109f94778ea3e44a9f92de6bdd4eb15384e488b3
Max Bachmann <kontakt@maxbachmann.de>
2022-06-22 16:23:35 +0200

    exclude cmake on arm machines (#224)

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Max Bachmann'
 author = 'Max Bachmann'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.12'
+release = '2.0.13'
 
 
 # -- General configuration ---------------------------------------------------
