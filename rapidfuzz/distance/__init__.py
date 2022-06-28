9fbf3e0534515dfef72f7120d10706734324d258
maxbachmann <kontakt@maxbachmann.de>
2022-06-28 22:39:19 +0200

    add strub for Editops/Opcodes

--- rapidfuzz/distance/__init__.py
+++ rapidfuzz/distance/__init__.py
@@ -1,6 +1,6 @@
 try:
-    from ._initialize_cpp import Editops, Opcodes, ScoreAlignment
+    from ._initialize_cpp import Editop, Editops, Opcode, Opcodes, ScoreAlignment
 except ImportError:
-    from ._initialize_py import ScoreAlignment
+    from ._initialize_py import Editop, Editops, Opcode, Opcodes, ScoreAlignment
 
 from . import Hamming, Indel, Jaro, JaroWinkler, Levenshtein, LCSseq
