bf31149f3c84d46de8f991f2aa10e3fb95d7db09
maxbachmann <kontakt@maxbachmann.de>
2022-06-30 14:14:53 +0200

    add global config

--- rapidfuzz/__init__.py
+++ rapidfuzz/__init__.py
@@ -1,8 +1,29 @@
 """
 rapid string matching library
 """
-__author__ = "Max Bachmann"
-__license__ = "MIT"
-__version__ = "2.1.0"
+__author__: str = "Max Bachmann"
+__license__: str = "MIT"
+__version__: str = "2.1.0"
+
+class RapidfuzzConfig:
+    def __init__(self):
+        self._allow_fallback = True
+
+    @property
+    def allow_fallback(self) -> bool:
+        return self._allow_fallback
+
+    @allow_fallback.setter
+    def allow_fallback(self, value: bool):
+        if value is False:
+            try:
+                # in rapidfuzz either all C++ versions are available or none
+                # so it is enough to validate one import
+                from rapidfuzz import fuzz_cpp
+            except ImportError:
+                raise ValueError("C++ implementation not available")
+        self._allow_fallback = value
+
+config: RapidfuzzConfig = RapidfuzzConfig()
 
 from rapidfuzz import process, distance, fuzz, string_metric, utils
