ff0f4bbd5776903ae821d6a818708a84c63b41ff
Max Bachmann <kontakt@maxbachmann.de>
2022-06-30 22:58:21 +0200

    add Jaro/JaroWinkler stub

--- rapidfuzz/distance/Jaro.py
+++ rapidfuzz/distance/Jaro.py
@@ -1,1 +1,6 @@
-from jarowinkler import jaro_similarity as similarity
+try:
+    from jarowinkler import jaro_similarity as similarity
+except ImportError:
+
+    def similarity(s1, s2, *, processor=None, score_cutoff=None):
+        raise NotImplementedError
--- rapidfuzz/distance/JaroWinkler.py
+++ rapidfuzz/distance/JaroWinkler.py
@@ -1,1 +1,6 @@
-from jarowinkler import jarowinkler_similarity as similarity
+try:
+    from jarowinkler import jarowinkler_similarity as similarity
+except ImportError:
+
+    def similarity(s1, s2, *, prefix_weight=0.1, processor=None, score_cutoff=None):
+        raise NotImplementedError
