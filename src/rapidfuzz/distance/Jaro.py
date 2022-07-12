2bbafa5f682d348416d87acfa3851b4d4778e3ed
Max Bachmann <kontakt@maxbachmann.de>
2022-07-13 00:13:45 +0200

    remove outdated fallback

--- src/rapidfuzz/distance/Jaro.py
+++ src/rapidfuzz/distance/Jaro.py
@@ -1,6 +1,1 @@
-try:
-    from jarowinkler import jaro_similarity as similarity
-except ImportError:
-
-    def similarity(s1, s2, *, processor=None, score_cutoff=None):
-        raise NotImplementedError
+from jarowinkler import jaro_similarity as similarity
--- src/rapidfuzz/distance/JaroWinkler.py
+++ src/rapidfuzz/distance/JaroWinkler.py
@@ -1,6 +1,1 @@
-try:
-    from jarowinkler import jarowinkler_similarity as similarity
-except ImportError:
-
-    def similarity(s1, s2, *, prefix_weight=0.1, processor=None, score_cutoff=None):
-        raise NotImplementedError
+from jarowinkler import jarowinkler_similarity as similarity
