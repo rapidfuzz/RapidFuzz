93b0124d7a07172c9f7309feb2d8316436297548
Max Bachmann <kontakt@maxbachmann.de>
2022-07-29 21:13:30 +0200

    release v2.4.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 ## Changelog
 
+### [2.4.1] - 2022-07-29
+#### Fixed
+- fix version number
+
 ### [2.4.0] - 2022-07-29
 #### Fixed
 - fix banded Levenshtein implementation
--- src/rapidfuzz/__init__.py
+++ src/rapidfuzz/__init__.py
@@ -3,6 +3,6 @@ rapid string matching library
 """
 __author__: str = "Max Bachmann"
 __license__: str = "MIT"
-__version__: str = "2.3.0"
+__version__: str = "2.4.1"
 
 from rapidfuzz import process, distance, fuzz, string_metric, utils
