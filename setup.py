4b5a4730d43a852e87f718788e938eb15a6a46f7
Max Bachmann <kontakt@maxbachmann.de>
2022-08-12 17:34:12 +0200

    add python 3.11 classifier

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup_args = {
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License"
     ],
 
