f4feaf79b8cf73706ec1db74a27115755dd11953
Max Bachmann <kontakt@maxbachmann.de>
2022-09-11 00:54:56 +0200

    release v2.7.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Max Bachmann'
 author = 'Max Bachmann'
 
 # The full version, including alpha/beta/rc tags
-release = '2.6.1'
+release = '2.7.0'
 
 
 # -- General configuration ---------------------------------------------------
