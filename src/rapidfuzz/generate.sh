e81b9e9c3b82574fe37c6a26ecc7f8d0269a0cfc
Max Bachmann <kontakt@maxbachmann.de>
2023-08-02 15:15:25 +0200

    improve x86 support (fixes #342)
    - build x86 with sse2/avx2 runtime detection
    - test for actual atomics used by taskflow

--- src/rapidfuzz/generate.sh
+++ src/rapidfuzz/generate.sh
@@ -9,6 +9,7 @@ generate_cython()
 
 generate_cython fuzz_cpp
 generate_cython fuzz_cpp_avx2
+generate_cython fuzz_cpp_sse2
 generate_cython process_cpp_impl
 generate_cython utils_cpp
 generate_cython _feature_detector_cpp
@@ -16,3 +17,4 @@ generate_cython _feature_detector_cpp
 generate_cython distance/_initialize_cpp
 generate_cython distance/metrics_cpp
 generate_cython distance/metrics_cpp_avx2
+generate_cython distance/metrics_cpp_sse2
