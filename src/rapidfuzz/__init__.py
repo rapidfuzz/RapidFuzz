6269c13ac8bd2684d90f7967eb50d07d78055baf
Max Bachmann <oss@maxbachmann.de>
2023-10-09 02:39:50 +0200

    fix edge case in simd implementation of Jaro/JaroWinkler

--- src/rapidfuzz/__init__.py
+++ src/rapidfuzz/__init__.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 
 __author__: str = "Max Bachmann"
 __license__: str = "MIT"
-__version__: str = "3.3.1"
+__version__: str = "3.4.0"
 
 from rapidfuzz import distance, fuzz, process, utils
 
