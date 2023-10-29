643846b95dd979b2492dfdc93dd2e109ea974d1a
Max Bachmann <oss@maxbachmann.de>
2023-10-29 21:34:38 +0100

    fix some warnings

--- tools/test_submodules.py
+++ tools/test_submodules.py
@@ -1,23 +1,25 @@
-import os
+from __future__ import annotations
+
+from pathlib import Path
+
 from git import Repo, cmd
 
 
 def lsremote(url):
     """
     only one way for
-    git ls-remote -t --exit-code --refs {url} | sed -E 's/^[[:xdigit:]]+[[:space:]]+refs\/tags\/(.+)/\1/g'
+    git ls-remote -t --exit-code --refs {url} | sed -E 's/^[[:xdigit:]]+[[:space:]]+refs\\/tags\\/(.+)/\1/g'
     """
     g = cmd.Git()
     refs = g.ls_remote("-t", "--exit-code", url).split("\n")
-    last_tag = refs[-1].split("\t")[-1].split("/")[-1]
-    return last_tag
+    return refs[-1].split("\t")[-1].split("/")[-1]
 
 
 def test_submodules():
     """
     find tag in current repo and check tag in remote submodule
     """
-    repo_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    repo_path = Path(__file__).resolve().parent.parent
     repo = Repo(repo_path)  # init class Repo
     assert not repo.bare  # checking if repo is bare
 
@@ -28,14 +30,13 @@ def test_submodules():
             (tag for tag in sub_repo.tags if tag.commit == sub_repo.head.commit), None
         )  # find current tag in local repo
 
-        assert current_tag != None, f"You don't use tag for '{module.name}'!"
+        assert current_tag is not None, f"You don't use tag for '{module.name}'!"
 
         current_tag = current_tag.name  # git.Tag to str
         remote_tag = lsremote(module.url)  # find last tag in remote module
 
-        assert (
-            current_tag == remote_tag
-        ), f"Local submodule tag {current_tag} does not match remote {remote_tag}"
+        assert current_tag == remote_tag, f"Local submodule tag {current_tag} does not match remote {remote_tag}"
+
 
 if __name__ == "__main__":
     test_submodules()
