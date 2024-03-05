e8cd0ed8cbaa0989a4349619f1aa2f02eae9a6f1
Julian Gilbey <jdg@debian.org>
2024-03-05 22:06:32 +0000

    Comment out sphinx config static directory as it does not exist

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ pygments_dark_style = "monokai"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
 
 napoleon_google_docstring = False
 
