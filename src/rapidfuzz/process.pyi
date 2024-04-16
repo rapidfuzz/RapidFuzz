66b78c8dd91d71a11ac064f526b14d920f61c932
Max Bachmann <oss@maxbachmann.de>
2024-04-16 12:40:38 +0200

    type scorer argument

--- src/rapidfuzz/process.pyi
+++ src/rapidfuzz/process.pyi
@@ -8,8 +8,10 @@ from typing import (
     Hashable,
     Iterable,
     Mapping,
+    Protocol,
     Sequence,
     TypeVar,
+    cast,
     overload,
 )
 
@@ -18,18 +20,29 @@ from rapidfuzz.fuzz import WRatio, ratio
 _StringType = Sequence[Hashable]
 _StringType1 = TypeVar("_StringType1", bound=Sequence[Hashable])
 _StringType2 = TypeVar("_StringType2", bound=Sequence[Hashable])
+_UnprocessedType1 = TypeVar("_UnprocessedType1")
+_UnprocessedType2 = TypeVar("_UnprocessedType2")
 _KeyType = TypeVar("_KeyType")
 _ResultType = TypeVar("_ResultType", int, float)
 
-_UnprocessedType1 = TypeVar("_UnprocessedType1")
-_UnprocessedType2 = TypeVar("_UnprocessedType2")
+_StringType1_contra = TypeVar("_StringType1_contra", contravariant=True, bound=Sequence[Hashable])
+_StringType2_contra = TypeVar("_StringType2_contra", contravariant=True, bound=Sequence[Hashable])
+_ResultType_contra = TypeVar("_ResultType_contra", int, float, contravariant=True)
+_ResultType_co = TypeVar("_ResultType_co", int, float, covariant=True)
+
+class _Scorer(Protocol[_StringType1_contra, _StringType2_contra, _ResultType_contra, _ResultType_co]):
+    def __call__(
+        self, __s1: _StringType1_contra, __s2: _StringType2_contra, *, score_cutoff: _ResultType_contra | None
+    ) -> _ResultType_co: ...
+
+_default_scorer: _Scorer[Any, Any, Any, Any] = cast(_Scorer[Any, Any, Any, Any], WRatio)
 
 @overload
 def extractOne(
     query: _StringType1 | None,
     choices: Mapping[_KeyType, _StringType2 | None],
     *,
-    scorer: Callable[..., _ResultType] = WRatio,
+    scorer: _Scorer[_StringType1, _StringType2, _ResultType, _ResultType] = _default_scorer,
     processor: None = None,
     score_cutoff: _ResultType | None = None,
     score_hint: _ResultType | None = None,
@@ -40,7 +53,7 @@ def extractOne(
     query: _StringType1 | None,
     choices: Iterable[_StringType2 | None],
     *,
-    scorer: Callable[..., _ResultType] = WRatio,
+    scorer: _Scorer[_StringType1, _StringType2, _ResultType, _ResultType] = _default_scorer,
     processor: None = None,
     score_cutoff: _ResultType | None = None,
     score_hint: _ResultType | None = None,
@@ -51,8 +64,8 @@ def extractOne(
     query: _UnprocessedType1 | None,
     choices: Mapping[_KeyType, _UnprocessedType2 | None],
     *,
-    scorer: Callable[..., _ResultType] = WRatio,
-    processor: Callable[[_UnprocessedType1 | _UnprocessedType2], Sequence[Hashable]],
+    scorer: _Scorer[_StringType1, _StringType1, _ResultType, _ResultType] = _default_scorer,
+    processor: Callable[[_UnprocessedType1 | _UnprocessedType2], _StringType1],
     score_cutoff: _ResultType | None = None,
     score_hint: _ResultType | None = None,
     scorer_kwargs: dict[str, Any] | None = None,
@@ -62,8 +75,8 @@ def extractOne(
     query: _UnprocessedType1 | None,
     choices: Iterable[_UnprocessedType2 | None],
     *,
-    scorer: Callable[..., _ResultType] = WRatio,
-    processor: Callable[[_UnprocessedType1 | _UnprocessedType2], Sequence[Hashable]],
+    scorer: _Scorer[_StringType1, _StringType1, _ResultType, _ResultType] = _default_scorer,
+    processor: Callable[[_UnprocessedType1 | _UnprocessedType2], _StringType1],
     score_cutoff: _ResultType | None = None,
     score_hint: _ResultType | None = None,
     scorer_kwargs: dict[str, Any] | None = None,
@@ -73,7 +86,7 @@ def extract(
     query: _StringType1 | None,
     choices: Mapping[_KeyType, _StringType2 | None],
     *,
-    scorer: Callable[..., _ResultType] = WRatio,
+    scorer: _Scorer[_StringType1, _StringType2, _ResultType, _ResultType] = _default_scorer,
     processor: None = None,
     limit: int | None = 5,
     score_cutoff: _ResultType | None = None,
@@ -85,7 +98,7 @@ def extract(
     query: _StringType1 | None,
     choices: Collection[_StringType2 | None],
     *,
-    scorer: Callable[..., _ResultType] = WRatio,
+    scorer: _Scorer[_StringType1, _StringType2, _ResultType, _ResultType] = _default_scorer,
     processor: None = None,
     limit: int | None = 5,
     score_cutoff: _ResultType | None = None,
@@ -97,8 +110,8 @@ def extract(
     query: _UnprocessedType1 | None,
     choices: Mapping[_KeyType, _UnprocessedType2 | None],
     *,
-    scorer: Callable[..., _ResultType] = WRatio,
-    processor: Callable[[_UnprocessedType1 | _UnprocessedType2], Sequence[Hashable]],
+    scorer: _Scorer[_StringType1, _StringType1, _ResultType, _ResultType] = _default_scorer,
+    processor: Callable[[_UnprocessedType1 | _UnprocessedType2], _StringType1],
     limit: int | None = 5,
     score_cutoff: _ResultType | None = None,
     score_hint: _ResultType | None = None,
@@ -109,8 +122,8 @@ def extract(
     query: _UnprocessedType1 | None,
     choices: Collection[_UnprocessedType2 | None],
     *,
-    scorer: Callable[..., _ResultType] = WRatio,
-    processor: Callable[[_UnprocessedType1 | _UnprocessedType2], Sequence[Hashable]],
+    scorer: _Scorer[_StringType1, _StringType1, _ResultType, _ResultType] = _default_scorer,
+    processor: Callable[[_UnprocessedType1 | _UnprocessedType2], _StringType1],
     limit: int | None = 5,
     score_cutoff: _ResultType | None = None,
     score_hint: _ResultType | None = None,
@@ -121,7 +134,7 @@ def extract_iter(
     query: _StringType1 | None,
     choices: Mapping[_KeyType, _StringType2 | None],
     *,
-    scorer: Callable[..., _ResultType] = WRatio,
+    scorer: _Scorer[_StringType1, _StringType2, _ResultType, _ResultType] = _default_scorer,
     processor: None = None,
     score_cutoff: _ResultType | None = None,
     score_hint: _ResultType | None = None,
@@ -132,7 +145,7 @@ def extract_iter(
     query: _StringType1 | None,
     choices: Iterable[_StringType2 | None],
     *,
-    scorer: Callable[..., _ResultType] = WRatio,
+    scorer: _Scorer[_StringType1, _StringType2, _ResultType, _ResultType] = _default_scorer,
     processor: None = None,
     score_cutoff: _ResultType | None = None,
     score_hint: _ResultType | None = None,
@@ -143,8 +156,8 @@ def extract_iter(
     query: _UnprocessedType1 | None,
     choices: Mapping[_KeyType, _UnprocessedType2 | None],
     *,
-    scorer: Callable[..., _ResultType] = WRatio,
-    processor: Callable[[_UnprocessedType1 | _UnprocessedType2], Sequence[Hashable]],
+    scorer: _Scorer[_StringType1, _StringType1, _ResultType, _ResultType] = _default_scorer,
+    processor: Callable[[_UnprocessedType1 | _UnprocessedType2], _StringType1],
     score_cutoff: _ResultType | None = None,
     score_hint: _ResultType | None = None,
     scorer_kwargs: dict[str, Any] | None = None,
@@ -154,8 +167,8 @@ def extract_iter(
     query: _UnprocessedType1 | None,
     choices: Iterable[_UnprocessedType2 | None],
     *,
-    scorer: Callable[..., _ResultType] = WRatio,
-    processor: Callable[[_UnprocessedType1 | _UnprocessedType2], Sequence[Hashable]],
+    scorer: _Scorer[_StringType1, _StringType1, _ResultType, _ResultType] = _default_scorer,
+    processor: Callable[[_UnprocessedType1 | _UnprocessedType2], _StringType1],
     score_cutoff: _ResultType | None = None,
     score_hint: _ResultType | None = None,
     scorer_kwargs: dict[str, Any] | None = None,
--- tools/test_process_typing.py
+++ tools/test_process_typing.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Generator
+from typing import Generator, Hashable, Sequence
 
 from rapidfuzz import fuzz, process
 from rapidfuzz.distance import Levenshtein
@@ -15,43 +15,103 @@ class MyClass:
         self.a: str = ""
 
 
+def valid_scorer_seq1(s1: Sequence[Hashable], s2: Sequence[Hashable], score_cutoff: float | None) -> float:
+    return 1.0
+
+
+def valid_scorer_str1(s1: str, s2: str, score_cutoff: float | None) -> float:
+    return 1.0
+
+
+def valid_scorer_str2(s1: str, s2: str, *, score_cutoff: float | None) -> float:
+    return 1.0
+
+
+def valid_scorer_str3(string1: str, string2: str, *, score_cutoff: float | None) -> float:
+    return 1.0
+
+
+def invalid_scorer_str1(s1: str, s2: str, cutoff: float | None) -> float:
+    return 1.0
+
+
+def invalid_scorer_str2(s1: str, s2: str) -> float:
+    return 1.0
+
+
+def invalid_scorer_str3(s1: str, s2: str, score_cutoff: int | None) -> float:
+    return 1.0
+
+
+def invalid_scorer_str4(s1: int, s2: str, score_cutoff: float | None) -> float:
+    return 1.0
+
+
+def invalid_scorer_str5(s1: str, s2: int, score_cutoff: float | None) -> float:
+    return 1.0
+
+
 def test_extractOne():
     a: tuple[str, float, int] = process.extractOne("", [""])
     a = process.extractOne("", [""], scorer=fuzz.ratio)
-    b: tuple[str, int, int] = process.extractOne("", [""], scorer=fuzz.ratio)  # type: ignore [arg-type]
+    b: tuple[str, int, int] = process.extractOne("", [""], scorer=fuzz.ratio)  # type: ignore [call-overload]
     b = process.extractOne("", [""], scorer=Levenshtein.distance)
     a = process.extractOne(1, [""], scorer=fuzz.ratio)  # type: ignore [call-overload]
-    a = process.extractOne("", [1], scorer=fuzz.ratio)  # type: ignore [list-item]
+    a = process.extractOne("", [1], scorer=fuzz.ratio)  # type: ignore [call-overload]
     a = process.extractOne(list(""), [""], scorer=fuzz.ratio)
     c: tuple[list[str], float, int] = process.extractOne("", [list("")], scorer=fuzz.ratio)
     a = process.extractOne("", [""], processor=string_preprocessor)
     a = process.extractOne(list(""), [""], processor=string_preprocessor)  # type: ignore [arg-type]
     c = process.extractOne("", [list("")], processor=string_preprocessor)  # type: ignore [arg-type]
     d: tuple[MyClass, float, int] = process.extractOne(MyClass(), [MyClass()], processor=lambda x: x.a)
+    a = process.extractOne("", [""], scorer=valid_scorer_str1)
+    a = process.extractOne("", [""], scorer=valid_scorer_str2)
+    a = process.extractOne("", [""], scorer=valid_scorer_str3)
+    a = process.extractOne("", [""], scorer=invalid_scorer_str1)  # type: ignore [call-overload]
+    a = process.extractOne("", [""], scorer=invalid_scorer_str2)  # type: ignore [call-overload]
+    a = process.extractOne("", [""], scorer=invalid_scorer_str3)  # type: ignore [call-overload]
+    a = process.extractOne("", [""], scorer=invalid_scorer_str4)  # type: ignore [call-overload]
+    a = process.extractOne("", [""], scorer=invalid_scorer_str5)  # type: ignore [call-overload]
+    a = process.extractOne(list(""), [""], scorer=valid_scorer_str1)  # type: ignore [call-overload]
+    c = process.extractOne("", [list("")], scorer=valid_scorer_str1)  # type: ignore [call-overload]
+    a = process.extractOne(list(""), [""], scorer=valid_scorer_seq1)
+    c = process.extractOne("", [list("")], scorer=valid_scorer_seq1)
 
 
 def test_extract():
     a: list[tuple[str, float, int]] = process.extract("", [""])
     a = process.extract("", [""], scorer=fuzz.ratio)
-    b: list[tuple[str, int, int]] = process.extract("", [""], scorer=fuzz.ratio)  # type: ignore [arg-type]
+    b: list[tuple[str, int, int]] = process.extract("", [""], scorer=fuzz.ratio)  # type: ignore [call-overload]
     b = process.extract("", [""], scorer=Levenshtein.distance)
     a = process.extract(1, [""], scorer=fuzz.ratio)  # type: ignore [call-overload]
-    a = process.extract("", [1], scorer=fuzz.ratio)  # type: ignore [list-item]
+    a = process.extract("", [1], scorer=fuzz.ratio)  # type: ignore [call-overload]
     a = process.extract(list(""), [""], scorer=fuzz.ratio)
     c: list[tuple[list[str], float, int]] = process.extract("", [list("")], scorer=fuzz.ratio)
     a = process.extract("", [""], processor=string_preprocessor)
     a = process.extract(list(""), [""], processor=string_preprocessor)  # type: ignore [arg-type]
     c = process.extract("", [list("")], processor=string_preprocessor)  # type: ignore [arg-type]
     d: list[tuple[MyClass, float, int]] = process.extract(MyClass(), [MyClass()], processor=lambda x: x.a)
+    a = process.extract("", [""], scorer=valid_scorer_str1)
+    a = process.extract("", [""], scorer=valid_scorer_str2)
+    a = process.extract("", [""], scorer=valid_scorer_str3)
+    a = process.extract("", [""], scorer=invalid_scorer_str1)  # type: ignore [call-overload]
+    a = process.extract("", [""], scorer=invalid_scorer_str2)  # type: ignore [call-overload]
+    a = process.extract("", [""], scorer=invalid_scorer_str3)  # type: ignore [call-overload]
+    a = process.extract("", [""], scorer=invalid_scorer_str4)  # type: ignore [call-overload]
+    a = process.extract("", [""], scorer=invalid_scorer_str5)  # type: ignore [call-overload]
+    a = process.extract(list(""), [""], scorer=valid_scorer_str1)  # type: ignore [call-overload]
+    c = process.extract("", [list("")], scorer=valid_scorer_str1)  # type: ignore [call-overload]
+    a = process.extract(list(""), [""], scorer=valid_scorer_seq1)
+    c = process.extract("", [list("")], scorer=valid_scorer_seq1)
 
 
 def test_extract_iter():
     a: Generator[tuple[str, float, int], None, None] = process.extract_iter("", [""])
     a = process.extract_iter("", [""], scorer=fuzz.ratio)
-    b: Generator[tuple[str, int, int], None, None] = process.extract_iter("", [""], scorer=fuzz.ratio)  # type: ignore [arg-type]
+    b: Generator[tuple[str, int, int], None, None] = process.extract_iter("", [""], scorer=fuzz.ratio)  # type: ignore [call-overload]
     b = process.extract_iter("", [""], scorer=Levenshtein.distance)
     a = process.extract_iter(1, [""], scorer=fuzz.ratio)  # type: ignore [call-overload]
-    a = process.extract_iter("", [1], scorer=fuzz.ratio)  # type: ignore [list-item]
+    a = process.extract_iter("", [1], scorer=fuzz.ratio)  # type: ignore [call-overload]
     a = process.extract_iter(list(""), [""], scorer=fuzz.ratio)
     c: Generator[tuple[list[str], float, int], None, None] = process.extract_iter("", [list("")], scorer=fuzz.ratio)
     a = process.extract_iter("", [""], processor=string_preprocessor)
@@ -60,3 +120,15 @@ def test_extract_iter():
     d: Generator[tuple[MyClass, float, int], None, None] = process.extract_iter(
         MyClass(), [MyClass()], processor=lambda x: x.a
     )
+    a = process.extract_iter("", [""], scorer=valid_scorer_str1)
+    a = process.extract_iter("", [""], scorer=valid_scorer_str2)
+    a = process.extract_iter("", [""], scorer=valid_scorer_str3)
+    a = process.extract_iter("", [""], scorer=invalid_scorer_str1)  # type: ignore [call-overload]
+    a = process.extract_iter("", [""], scorer=invalid_scorer_str2)  # type: ignore [call-overload]
+    a = process.extract_iter("", [""], scorer=invalid_scorer_str3)  # type: ignore [call-overload]
+    a = process.extract_iter("", [""], scorer=invalid_scorer_str4)  # type: ignore [call-overload]
+    a = process.extract_iter("", [""], scorer=invalid_scorer_str5)  # type: ignore [call-overload]
+    a = process.extract_iter(list(""), [""], scorer=valid_scorer_str1)  # type: ignore [call-overload]
+    c = process.extract_iter("", [list("")], scorer=valid_scorer_str1)  # type: ignore [call-overload]
+    a = process.extract_iter(list(""), [""], scorer=valid_scorer_seq1)
+    c = process.extract_iter("", [list("")], scorer=valid_scorer_seq1)
