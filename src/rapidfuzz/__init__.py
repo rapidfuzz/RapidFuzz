d176f65368f9cef119d46d39fca1e8ba681467cb
Max Bachmann <kontakt@maxbachmann.de>
2024-08-06 22:44:57 +0200

    tag release

--- src/rapidfuzz/__init__.py
+++ src/rapidfuzz/__init__.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 
 __author__: str = "Max Bachmann"
 __license__: str = "MIT"
-__version__: str = "3.9.5"
+__version__: str = "3.9.6"
 
 from rapidfuzz import distance, fuzz, process, utils
 
