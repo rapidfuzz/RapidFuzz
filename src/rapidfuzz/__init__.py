6c0b80a0c58b0abbf82941dcbe25e09f6487f86c
Max Bachmann <oss@maxbachmann.de>
2024-09-21 18:28:37 +0200

    update changelog

--- src/rapidfuzz/__init__.py
+++ src/rapidfuzz/__init__.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 
 __author__: str = "Max Bachmann"
 __license__: str = "MIT"
-__version__: str = "3.9.7"
+__version__: str = "3.10.0"
 
 from rapidfuzz import distance, fuzz, process, utils
 
