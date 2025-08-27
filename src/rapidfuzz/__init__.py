da7e03b0a1245551f67b80ded636c5a440df8225
Max Bachmann <max.bachmann@iracing.com>
2025-08-27 14:06:14 +0200

    tag version

--- src/rapidfuzz/__init__.py
+++ src/rapidfuzz/__init__.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 
 __author__: str = "Max Bachmann"
 __license__: str = "MIT"
-__version__: str = "3.13.0"
+__version__: str = "3.14.0"
 
 from rapidfuzz import distance, fuzz, process, utils
 
